94c6f8fd086d93c22994193885e0776fee6c40b3
Ranjeet <ranjeeturd@gmail.com>
2026-01-27 17:02:00 +0530

    comment event and department slice import and use for now

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducers from "@/store/slices/authSlice";
-import departmentReducers from "@/store/slices/departmentSlice";
-import eventReducers from "@/store/slices/eventsSlice";
+// import departmentReducers from "@/store/slices/departmentSlice";
+// import eventReducers from "@/store/slices/eventsSlice";
 
 export const rootReducer = combineReducers({
   auth: authReducers,
-  departments: departmentReducers,
-  events: eventReducers,
+  // departments: departmentReducers,
+  // events: eventReducers,
 });
