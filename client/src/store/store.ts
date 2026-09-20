a3a353913aac9e2b1e2d422af07d8c8d040937b3
Ranjeet Gupta <ranjeeturd@gmail.com>
2026-09-20 21:55:24 +0530

    update student dashboard into store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -3,10 +3,12 @@ import authReducers from "@/store/slices/authSlice";
 import departmentReducers from "@/store/slices/departmentSlice";
 import eventReducers from "@/store/slices/eventsSlice";
 import dashboardReducers from "@/store/slices/dashboardSlice";
+import studentReducers from "@/store/slices/studentSlice";
 
 export const rootReducer = combineReducers({
   auth: authReducers,
   departments: departmentReducers,
   events: eventReducers,
   dashboard: dashboardReducers,
+  student: studentReducers,
 });
